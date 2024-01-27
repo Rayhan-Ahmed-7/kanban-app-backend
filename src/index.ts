393aa4f628fdb763e4c25d88cdfede70d3948562
Rayhan-Ahmed-7 <dev.rayhan7@gmail.com>
2024-01-27 19:16:25 +0600

    :zap: initial steps done

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,13 @@
 import expreess, { Request, Response } from "express";
 import dotenv from "dotenv";
+import Server from "./server";
 
 dotenv.config()
 
 
-const app = expreess()
-const port = process.env.PORT;
-
-app.use(expreess.json())
+const app = expreess();
+const server: Server = new Server(app);
+const port = process.env.PORT || 8080;
 
 app.get("/", (req: Request, res: Response) => {
     res.send("hello world")
@@ -15,4 +15,11 @@ app.get("/", (req: Request, res: Response) => {
 
 app.listen(port, () => {
     console.log(`[server]: Server is running at http://localhost:${port}`);
-});
+})
+    .on("error", (err: any) => {
+        if (err.code === "EADDRINUSE") {
+            console.log("Error: address already in use");
+        } else {
+            console.log(err);
+        }
+    });
