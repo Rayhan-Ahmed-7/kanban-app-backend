2d3e78225a3d11f29d25b0c5f0f8c7a4be155a9b
Rayhan-Ahmed-7 <dev.rayhan7@gmail.com>
2024-02-06 19:43:43 +0600

    :zap: register validation improved

--- src/feature/auth/validation/authValidation.ts
+++ src/feature/auth/validation/authValidation.ts
@@ -3,7 +3,21 @@ import User from "../models/user_model"
 
 
 export const authValidation = [
-    body("username").isLength({ min: 8 }).withMessage("username must be at least 8 characters"),
+    body("username")
+        .notEmpty()
+        .withMessage("username is required.")
+        .bail()
+        .isLength({ min: 8 })
+        .withMessage("username must be at least 8 characters")
+        .custom(async (value) => {
+            // Check if a user with the provided username already exists
+            const existingUser = await User.findOne({ username: value });
+            if (existingUser) {
+                // If user already exists, reject the promise with an error message
+                return Promise.reject("User already exists with this username.");
+            }
+            // If user doesn't exist, resolve the promise
+        }),
     body("password").isLength({ min: 8 }).withMessage("password must be at least 8 characters"),
     // body('username').custom(value => {
     //     User.findOne({ username: value }).then(user => {
