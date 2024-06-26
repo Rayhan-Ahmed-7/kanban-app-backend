fbb08d6580b6c1d7d3a087f06660d8bb14a2dbda
Rayhan-Ahmed-7 <dev.rayhan7@gmail.com>
2024-06-27 00:16:37 +0600

    :zap: local network enabled inside cors

--- src/server.ts
+++ src/server.ts
@@ -4,26 +4,25 @@ import Routes from "./routes";
 import cookieParser from "cookie-parser";
 import db from "./config/db";
 
-
 class Server {
-    constructor(app: Application) {
-        this.config(app);
-        new Routes(app);
-    }
+  constructor(app: Application) {
+    this.config(app);
+    new Routes(app);
+  }
 
-    private config(app: Application): void {
-        const corsOptions: CorsOptions = {
-            origin: "http://localhost:5173",
-            credentials: true
-        }
+  private config(app: Application): void {
+    const corsOptions: CorsOptions = {
+      origin: ["http://localhost:5173", "http://192.168.10.183:5173"],
+      credentials: true,
+    };
 
-        app.use(cors(corsOptions));
-        app.use(express.json());
-        app.use(cookieParser());
-        app.use(express.urlencoded({ extended: false }));
+    app.use(cors(corsOptions));
+    app.use(express.json());
+    app.use(cookieParser());
+    app.use(express.urlencoded({ extended: false }));
 
-        db.connect();
-    }
+    db.connect();
+  }
 }
 
 export default Server;
