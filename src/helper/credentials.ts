8f0434f9f499b455f7d5440d32fbcfd0c1153a1a
Rayhan-Ahmed-7 <dev.rayhan7@gmail.com>
2024-06-29 14:28:09 +0600

    :zap: vercel json added

--- src/helper/credentials.ts
+++ src/helper/credentials.ts
@@ -1,11 +1,11 @@
-import dotenv from 'dotenv';
+import dotenv from "dotenv";
 dotenv.config({
-    path:"src/config/.env"
-})
-class AppCredentials{
-    static PORT = process.env.PORT || 8000;
-    static DB_URL = process.env.DB_URL || '';
-    static JWT_SECRET = process.env.JWT_SECRET || '';
+  path: "src/config/.env",
+});
+class AppCredentials {
+  static PORT = process.env.PORT || 8000;
+  static DB_URL = process.env.DB_LIVE_URL || "";
+  static JWT_SECRET = process.env.JWT_SECRET || "";
 }
 
-export default AppCredentials; 
+export default AppCredentials;
--- src/index.ts
+++ src/index.ts
@@ -2,23 +2,21 @@ import expreess, { Request, Response } from "express";
 import Server from "./server";
 import AppCredentials from "./helper/credentials";
 
-
-
-
 const app = expreess();
 const server: Server = new Server(app);
 const port = AppCredentials.PORT;
 
 app.get("/", (req: Request, res: Response) => {
-    res.send("hello world")
-})
-app.listen(port, () => {
+  res.send("hello world");
+});
+app
+  .listen(port, () => {
     console.log(`[server]: Server is running at http://localhost:${port}`);
-})
-    .on("error", (err: any) => {
-        if (err.code === "EADDRINUSE") {
-            console.log("Error: address already in use");
-        } else {
-            console.log(err);
-        }
-    });
+  })
+  .on("error", (err: any) => {
+    if (err.code === "EADDRINUSE") {
+      console.log("Error: address already in use");
+    } else {
+      console.log(err);
+    }
+  });
